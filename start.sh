81ecd50a13d0f7ca47b261f98b5887d64de6f92d
rulkimi <azrulkimi00@gmail.com>
2025-09-08 01:16:21 +0800

    change to ipv6

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
 echo "Running Alembic migrations..."
 alembic upgrade head
 
-echo "Starting FastAPI..."
-uvicorn main:app --host 0.0.0.0 --port ${PORT}
+echo "Starting FastAPI on IPv6..."
+uvicorn main:app --host :: --port ${PORT}
