9de2eeabd020aaa2509c512036ab80e86bf9493c
rulkimi <azrulkimi00@gmail.com>
2025-09-08 00:54:05 +0800

    fix: port

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Running Alembic migrations..."
 alembic upgrade head
 
 echo "Starting FastAPI..."
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
+uvicorn main:app --host 0.0.0.0 --port ${PORT}
